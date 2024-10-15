931d032437b637e786d70ddc915000b192a160e3
Andres <andres.nunez01@outlook.com>
2024-10-14 19:24:08 -0600

    dev-deployment-destroy

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "NetworkWatcherRG"
+    storage_account_name = "711incidentandres"
+    container_name = "711-75884-tfstate"
+    key = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,6 +12,7 @@ terraform {
     }
   }
 }
+
 
 
 provider "azurerm" {
